2a064bb4ae3d1b3ea19f6f308d1c52d543236156
liormilliger <some@email.com>
2025-02-11 07:44:53 +0200

    changes to ./data/database.js file and reverting workflow

--- data/database.js
+++ data/database.js
@@ -1,12 +1,13 @@
 import { MongoClient } from 'mongodb';
 
 const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
-const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+// const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const clusterAddress = process.env.MONGODB_IP_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}:27017/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
